04d544dac8b708afd90dc789398f95d421a9a864
aablanquicet27 <aablanquicet@gmail.com>
2026-07-31 01:08:33 +0000

    v3: mockup del celular inmediatamente despues del hero

---
+++ _next/static/cszUywiTQyF5N5aEtmo2x/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/club-profesionales-propuesta//_next/:path+",
+        "destination": "/club-profesionales-propuesta/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
